6de8d7250df31b42010c4e5b7901b1ddb8722067
Abi Beggs <beggsaj@gmail.com>
2021-11-08 07:30:13 -0600

    added local storage functionality and outline for final logic

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
 // display current date
 var currentDay = moment().format("dddd LL");
 $("#currentDay").append(currentDay);
+
+$document.ready(function () {
+    //saveBtn click listener
+    $(".saveBtn").on("click", function () {
+        //get value of description in jquery
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        //save to local storage
+
+        localStorage.setItem(time, text);
+    })
+
+    function timeTracker() {
+        //get current number of hours
+        var timeNow = moment().hour();
+
+        //loop over the time blocks
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+            //check time and update class for background colors
+            if (blockTime < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+
+            else if (blockTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+    }
+})
+
+//get item from local storage
+$("#8am .description").val(localStorage.getItem("8am"));
